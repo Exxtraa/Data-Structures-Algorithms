1ad56c11655db0b16b8289c919e690ea6f89e30e
Arpit <arpitsharma6545@gmail.com>
2025-05-09 15:25:17 +0530

    vectors with iterators

--- stl.cpp
+++ stl.cpp
@@ -2,17 +2,45 @@
 using namespace std;
 
 //Algorithm
-//containers
+//containers                          Deatiled STL is this 
 //Fuctions
 //Iterators
 
-int definePair(){
-    pair<int,pair<int,int>> p = {1,{8,6}};
-    cout << p.second.first;
+
+
+// 1. PAIR
+// int definePair(){
+//     pair<int,int> p = {2,8};
+//     cout << p.second;
+
+//     pair<int ,int> p[] = {{2,4},{56,8},{6,9}};
+//     cout << p[1].first;
+// }
+
+
+
+// 2.  VECTOR
+int defineVectors(){
+    vector <int> v;
+    v.push_back(5);
+
+    vector <int>::iterator it = v.begin();
+    
+    it++;
+    cout << *(it) << " ";
+
+    it+ 2;
+    cout << *(it) << " ";
+
+    // vector<int>::iterator it-- = v.end();  Cuz without -- it takes value out of the array so to be in array use --
+    vector<int>::iterator it = v.rend();
+    vector<int>::iterator it = v.rbegin();
 }
 
+
 int main (){
-    definePair();
+    // definePair();
+    defineVectors();
 }
 
 
