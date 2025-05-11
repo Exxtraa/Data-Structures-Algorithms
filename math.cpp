35191002ce4c874eeb53ee46c55b13b42876d504
Arpit <arpitsharma6545@gmail.com>
2025-05-11 15:57:58 +0530

    prime and all with better time complaxity

--- math.cpp
+++ math.cpp
@@ -86,6 +86,26 @@ using namespace std;
 // }
 
 
+//PRIME NUMBER
+int prime(){
+    int n = 11;
+	int count = 0;
+	for(int i=1; i*i<=n; i++){
+		if(n % i == 0){
+			count++;
+			if((n/i) != i){
+				count++;
+			};
+		};
+	};
+	if( count == 2){
+		 cout << "true";
+	} else{
+		 cout << "false";
+	};
+}
+
+
 
 int main(){
     // reverse();
@@ -94,5 +114,6 @@ int main(){
     // armstrong();
     // divison();
     // divisonAll();
-    
+    prime();
+    return 0;
 }
