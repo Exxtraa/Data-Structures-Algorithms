aa2151a9b2ce3c3648d81ac3a7c3fdd69d252805
Arpit <arpitsharma6545@gmail.com>
2025-05-11 14:30:48 +0530

    palindrome,count digit,armstrong,divison with till its root

--- math.cpp
+++ math.cpp
@@ -1,5 +1,6 @@
 # include<bits/stdc++.h>
 using namespace std;
+#include <cmath>
 
 //     REVERSE AN INTEGER
 // int reverse(){
@@ -23,27 +24,75 @@ using namespace std;
 //     };
 //     cout << count; }
 
+// PALINDROME LIKE IF WE REVERSED THE NUBMER IT'S SAME AS INPUT SO CHECK TRUE OR NOT
+// int palindrome(){
+//     int x = 1234567899;
+//     int rev = 0;
+//     int dup = x;
+//     if(x < 0) cout << "false";
+//     while( x != 0){
+//         int ld = x % 10;
+//         rev = (rev * 10) + ld;
+//         x /= 10;
+//     };
+//     if( dup == rev) cout << "true";
+//     else cout << "false"; }
 
-int palindrome(){
-    int x = 22;
-    int dup = x;
-    int rev =0;
-    while( x != 0){
-        int ld = x % 10;
-        rev = (rev *10 )+ ld;
-        x /=10;
-    };
-    bool check = dup == rev ;
-    if (check){
-        cout << "palindrome true";
-    } else{
-        cout << "palindrome false";
-    };
 
-}
+// ARMSTRONG IN THIS 134 = IT WILL MULTIPY BY 134TO 3 CUBE TO EVERY SINGLE NUMBER ADN THE OUTPUT WOULD BE SAME AS INPUT
+// int armstrong(){
+//     int x = 1634;
+//     int temp = x;
+//     int r;
+//     int sum = 0;
+//     while( x > 0){
+//         r = x % 10;
+//         sum = sum+(r*r*r);
+//         x = x/10;
+//     };
+//     if(temp == sum){
+//         cout << "armstrong";
+//     }else{
+//         cout << "not an armstrong ";
+//     };
+// }
+
+// ALL DIVISON  
+// int divison(){
+//     int x = 10; // oputput = 1 2 5 10 //
+//     for(int i = 1; i <= x; i++){
+//         if ( x % i == 0){
+//             cout << i << " ";
+//         };
+//     };
+// }
+
+// ALL DIVISON WITH STL ADN SORTING
+// int divisonAll(){
+//     int x = 36;
+//     vector<int> is;
+//     for(int i = 1; i <= sqrt(x); i++){
+//         if ( x % i == 0){
+//             is.push_back(i);
+//             if((x/i) != i){
+//                 is.push_back(x/i);
+//             }
+//         };
+//     };
+//     sort(is.begin(),is.end());
+//     for(auto it : is){
+//         cout << it << " ";
+//     }
+// }
+
+
 
 int main(){
     // reverse();
     // countDigit()
-    palindrome();
+    // palindrome();
+    // armstrong();
+    // divison();
+    // divisonAll();
+    
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,16 +1,5 @@
-bool comparitor(pair<int,int>p1 , pair<int,int>p2){
-    if(p1.second<p2.second)return true;
-    else return false;
-};
-
-int definePairSorting(){
-    vector<pair<int,int>> v = {{5,6,}{8,9}{4,3}};
-
-    sort(v.begin(), v.end(),comparitor);
-
-    for(auto l : v){
-        cout << l.first << " " << l.second << endl;
-    }
-
-    return 0;
-}
+    // if( arm == dup){
+    //     cout << "it is armstrong";
+    // } else{
+    //     cout << "it is not armstrong";
+    // };
