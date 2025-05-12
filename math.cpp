185f8088b5917dcd9a564ae10d82e2e009beb6a4
Arpit <arpitsharma6545@gmail.com>
2025-05-12 09:15:39 +0530

    recursion with little bcaktaking

--- math.cpp
+++ math.cpp
@@ -87,23 +87,23 @@ using namespace std;
 
 
 //PRIME NUMBER
-int prime(){
-    int n = 11;
-	int count = 0;
-	for(int i=1; i*i<=n; i++){
-		if(n % i == 0){
-			count++;
-			if((n/i) != i){
-				count++;
-			};
-		};
-	};
-	if( count == 2){
-		 cout << "true";
-	} else{
-		 cout << "false";
-	};
-}
+// int prime(){
+//     int n = 11;
+// 	int count = 0;
+// 	for(int i=1; i*i<=n; i++){
+// 		if(n % i == 0){
+// 			count++;
+// 			if((n/i) != i){
+// 				count++;
+// 			};
+// 		};
+// 	};
+// 	if( count == 2){
+// 		 cout << "true";
+// 	} else{
+// 		 cout << "false";
+// 	};
+// }
 
 
 
@@ -114,6 +114,6 @@ int main(){
     // armstrong();
     // divison();
     // divisonAll();
-    prime();
-    return 0;
+    // prime();
+    // return 0;
 }
---
+++ recursion.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 1. PRINT N NUMBER TIME AND NUMBER TILL N BOTH CAN BE DONE
+// void print(int i ,int n){
+//     if( i > n) return; // <
+//     cout << i << endl;
+//     print(i+1,n); // i- 1 do both these if want reverse
+// }
+
+
+// 2. FROM BACKTRAKING 5 4 3 2 1
+// void printBack( int n){
+//     if( n == 0)return;
+//     cout << n << endl;
+//     print(n-1);
+// }
+
+int main(){
+    // 1.
+    // print(1,7);
+
+    // 2.
+    // int n =5;
+    // printBack(n);
+}
