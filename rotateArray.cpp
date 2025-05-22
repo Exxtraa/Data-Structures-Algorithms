594862be3cbbe0698a128a16a9f99b277105a01b
Arpit <arpitsharma6545@gmail.com>
2025-05-22 16:45:24 +0530

    array problem 2

---
+++ rotateArray.cpp
@@ -0,0 +1,26 @@
+
+
+
+//array II
+void sorted(vector<int>&arr,int k){
+    int n = arr.size();
+
+    k = k % n;
+
+    vector<int> temp(k);
+    for(int i =0;i<k;i++){
+        temp[i] = arr[i];
+    }
+    for(int i =k;i<n;i++){
+        arr[i - k] = arr[i];
+    }
+    for(int i = n-k;i<n;i++){
+        arr[i] = temp[i -(n-k)];
+    }
+}
+
+vector<int> rotateArray(vector<int>arr, int k) {
+    sorted(arr,k);
+    return arr;
+    
+}
